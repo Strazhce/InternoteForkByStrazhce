9d459446b0eefb55b81ef462f93e4ca2dc4a8d7e
codemachine <codemachine@73300841-0615-0410-aa3c-df2815646e3a>
2011-01-08 14:18:46 +0000

    Improve commenting of coordinate utils.

--- extension/chrome/content/internote-utils-coords.js
+++ extension/chrome/content/internote-utils-coords.js
@@ -16,49 +16,62 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+// This file contains various utilities for calculating with coordinates,
+// represented as size-2 arrays (pairs) of numbers.
+
 internoteSharedGlobal_e3631030_7c02_11da_a72b_0800200c9a66.utils.incorporate("CoordUtils", {
 
+// PUBLIC: Add two coordinates.
 coordPairAdd: function(a, b)
 {
     return [a[0] + b[0], a[1] + b[1]];
 },
 
+// PUBLIC: Subtract two coordinates.
 coordPairSubtract: function(a, b)
 {
     return [a[0] - b[0], a[1] - b[1]];
 },
 
+// PUBLIC: Subtract two coordinates, clipping to zero if negative
+// This prevents negative lengths causing issues in boundary cases.
 coordPairSubtractNonNeg: function(a, b)
 {
     return [Math.max(0, a[0] - b[0]), Math.max(0, a[1] - b[1])];
 },
 
+// PUBLIC: Multiply a coordinate by a constant.
 coordPairMultiply: function(c, a)
 {
     return [c * a[0], c * a[1]];
 },
 
+// PUBLIC: Take the pairwise minimum of two coordinates.
 coordPairMin: function(a, b)
 {
     return [Math.min(a[0], b[0]), Math.min(a[1], b[1])];
 },
 
+// PUBLIC: Take the pairwise maximum of two coordinates.
 coordPairMax: function(a, b)
 {
     return [Math.max(a[0], b[0]), Math.max(a[1], b[1])];
 },
 
+// PUBLIC: Check whether two coordinate pairs are equal.
 areCoordPairsEqual: function(a, b)
 {
     return a[0] == b[0] && a[1] == b[1];
 },
 
+// PUBLIC: Round off a coordinate.
 roundCoordPair: function(pair)
 {
     return [Math.round(pair[0]), Math.round(pair[1])];
 },
 
-// Linear interpolator between two points.
+// PUBLIC: Linear interpolator between two points.
+// proportion = 0.0 => pair1, proportion = 1.0 => pair2
 interpolateCoordPair: function(proportion, pair1, pair2)
 {
     this.assertError(this.isCoordPair(pair1),  "Invalid pair1.", pair1);
@@ -68,26 +81,31 @@ interpolateCoordPair: function(proportion, pair1, pair2)
             this.interpolate(proportion, pair1[1], pair2[1])];
 },
 
+// PUBLIC: Calculate distance between two coordinates.
 coordPairDistance: function(pair)
 {
     return Math.sqrt(pair[0] * pair[0] + pair[1] * pair[1]);
 },
 
+// PUBLIC: Check whether an object is a coordinate.
 isCoordPair: function(pair)
 {
     return this.isPair(pair) && this.isFiniteNumber(pair[0]) && this.isFiniteNumber(pair[1]);
 },
 
+// PUBLIC: Check whether an object is a non-negative coordinate in both dimensions.
 isNonNegCoordPair: function(pair)
 {
     return this.isCoordPair(pair) && pair[0] >= 0 && pair[1] >= 0;
 },
 
+// PUBLIC: Check whether an object is a positive coordinate in both dimensions.
 isPositiveCoordPair: function(pair)
 {
     return this.isCoordPair(pair) && pair[0] > 0 && pair[1] > 0;
 },
 
+// PUBLIC: Return the coordinate, clipped to zero in each dimension if negative.
 clipNegativeCoordPair: function(pair)
 {
     return this.coordPairMax([0, 0], pair);
